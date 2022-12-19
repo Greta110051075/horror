e19e9bd589776dd824c0a83ce3442b01b66be5de
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2022-12-19 20:08:15 +0800

    12-19-2

---
+++ horror/Assets/Scripts/DialogueData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace greta
+{
+    [CreateAssetMenu(menuName ="horror/Dialogue Data", fileName ="New Dialogue Data")]
+    public class DialogueData : ScriptableObject
+    {
+        [Header("對話者名稱")]
+        public string dialogueName;
+        [Header("對話內容"), TextArea(2, 10)]
+        public string[] dialogueContents;
+    }
+}
+
+
