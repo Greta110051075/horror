691aa9617f66e6e1e66e6dd83234fad18feb7891
Greta110051075 <113588842+Greta110051075@users.noreply.github.com>
2023-01-05 18:15:13 +0800

    1-5

---
+++ horror/Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace greta
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundSystem : MonoBehaviour
+    {
+        private AudioSource aud;
+
+        private void Awake()
+        {
+            aud = GetComponent<AudioSource>();
+        }
+
+        public void PlaySound(AudioClip sound) 
+        {
+            aud.PlayOneShot(sound);
+        }
+    }
+}
+
